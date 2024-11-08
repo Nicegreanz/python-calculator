d3a79b49dff8e1758d9f1b0ba1b7bf6e5cd45ac0
Nicegreanz <70066609+Nicegreanz@users.noreply.github.com>
2024-11-08 16:30:06 +0000

    Finish,added test case for calculator.

--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,33 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+        #test add()
+    def test_add_negative_number(self):
+        self.assertEqual(self.calc.add(-3,-2),-5)
+
+        #test subtract()
+    def test_subtract_positive(self):
+        self.assertEqual(self.calc.subtract(7,2),5)
+    def test_subtract_result_negative(self):
+        self.assertEqual(self.calc.subtract(7,8),-1)
+
+        # Test multiply()
+    def test_multiply_by_zero(self):
+        self.assertEqual(self.calc.multiply(10, 0), 0)
+    def test_multiply_positive_numbers(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+
+    # Test divide()
+    def test_divide_evenly(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+    def test_divide_with_remainder(self):
+        self.assertEqual(self.calc.divide(7, 2), 3)  # round number expected
+
+    # Test modulo()
+    def test_modulo_no_remainder(self):
+        self.assertEqual(self.calc.modulo(10, 2), 0)
+    def test_modulo_with_remainder(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
 if __name__ == '__main__':
     unittest.main()
